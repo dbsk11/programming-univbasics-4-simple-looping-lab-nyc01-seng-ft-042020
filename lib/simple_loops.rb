6501dacdf7d7f61bd2b283657de489e6be3004f7
balanced-widget-4002 <jessikim807@gmail.com>
2020-04-08 13:59:42 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -26,7 +26,7 @@ end
 
 def return_string_array(array)
   counter = 0
-  new_array = [ ]
+  new_array = []
   while counter < array.length do
     new_array.push(array[counter].to_s)
     counter += 1
