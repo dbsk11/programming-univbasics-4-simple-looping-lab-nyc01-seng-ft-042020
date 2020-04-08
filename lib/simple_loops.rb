e6625373802161f09298396e34415094498e5fd8
balanced-widget-4002 <jessikim807@gmail.com>
2020-04-08 13:49:19 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,6 +1,16 @@
 def loop_message_five_times(string)
   counter = 0
+
   while counter <=5 do
+    puts string
+    counter += 1
+  end
+end
+
+def loop_message_n_times(string, integer)
+  counter = integer
+
+  while counter <= integer do
     puts string
     counter += 1
   end
